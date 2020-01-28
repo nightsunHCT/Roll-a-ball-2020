a4802f0e0905a86103db4766897ab80eb2787e58
Helena <nightsunHCT@users.noreply.github.com>
2020-01-28 02:50:34 -0500

    add walls + camera

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -17,9 +17,16 @@ public class PlayerController : MonoBehaviour
     {
         float moveHorizontal = Input.GetAxis("Horizontal"); //  input --> connecting keyboard movement to the location of the ball 
         float moveVertical = Input.GetAxis("Vertical");
-
+            
         Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
 
+        if (Input.GetKeyDown("space") && GetComponent<Rigidbody>().transform.position.y <= .5f)
+        {
+            Vector3 jump = new Vector3(0.0f, 150.0f, 0.0f);
+
+            GetComponent<Rigidbody>().AddForce(jump);
+        }
+        
         rb.AddForce(movement * speed);
     }
 }
